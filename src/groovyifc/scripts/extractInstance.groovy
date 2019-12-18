d2c9e737a11305a4828c7c46960fac48b1951120
hlg <public.void@gmx.de>
2019-12-18 13:39:19 +0100

    script to extract instances

---
+++ src/groovyifc/scripts/extractInstance.groovy
@@ -0,0 +1,60 @@
+// usage: groovy extractInstance.groovy <input.ifc> #<entity number> <extracted.ifc>
+
+if (args.size() < 3) {
+  println "usage: groovy extractInstance.groovy <input.ifc> #<entity number> <output.ifc>"
+  System.exit(1)
+}
+
+def pattern = /#[0-9]+/
+table = [:]
+include = [] as Set
+
+new File(args[0]).eachLine{ line ->
+  def matches = (line =~ pattern).collect{ it as String } // workaround failing groovy magic
+  if (!matches) return
+  table[matches[0]] = [line, matches.size() > 1 ? matches[1..-1] : []]
+}
+
+def traverseRefs(root, todo){
+  assert table[root]
+  todo(root)
+  table[root][1].each{ ref ->
+    traverseRefs(ref, todo)
+  }
+}
+
+args[1].split(',').each{ searched ->
+  if (table[searched]) {
+    traverseRefs(searched, {no -> include << no})
+    println "Finished extracting entity $searched"
+  } else {
+    println "Enitity $searched not found in ${args[0]}"
+  }
+}
+writeExtract()
+
+def writeExtract(){
+  def file = new File(args[2])
+  def date = new Date().format("yyyy-MM-dd'T'HH:mm:ss", TimeZone.getTimeZone("UTC")) 
+  file.withWriter{ writer ->
+    writer << """ISO-10303-21;
+HEADER;
+FILE_DESCRIPTION(('Extracted entity'),'2;1');
+FILE_NAME('$file.name','$date',('',''),(''),'','','');
+FILE_SCHEMA(('IFC4'));
+ENDSEC;
+
+DATA;
+""".denormalize()
+    include = include.sort{ Integer.valueOf(it[1..-1])}
+    def extracted = include.collect{ table[it][0] }
+    include.eachWithIndex{ no, i -> (0..extracted.size-1).each{ k -> extracted[k] = extracted[k].replaceAll( "$no(\\D)","#${i+1}\$1") } } 
+    extracted.each{ writer.writeLine(it) }
+    writer << 'ENDSEC;\nEND-ISO-10303-21;'
+  }
+} 
+
+// TODO: extract IFC version from original file
+// TODO: extract specific instances via some GPath-like syntax, needs proper parser
+
+
