643780ac4c2b1f2f0a3b23844d473d991235493d
hlg <public.void@gmx.de>
2020-01-09 12:04:08 +0100

    strong randomness for GUIDS

--- src/groovyifc/utils/uniqueId.groovy
+++ src/groovyifc/utils/uniqueId.groovy
@@ -1,12 +1,24 @@
-def charset = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_$'
+import java.util.UUID
 
-new Random().with{
-   println charset[ nextInt() & 3 ] + (1..21).collect{ charset[ nextInt() & 63 ] }.join()  
+charset = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_$'
+
+def weakRandom_1(){
+  new Random().with{
+     charset[ nextInt() & 3 ] + (1..21).collect{ charset[ nextInt() & 63 ] }.join()  
+  }
 }
 
-byte[] bytes = new byte[22]
-new Random().nextBytes( bytes )
-println charset[ bytes[0]&3 ] + bytes[1..21].collect{ charset[ it&63 ] }.join()
+def weakRandom_2(){
+  byte[] bytes = new byte[22]
+  new Random().nextBytes( bytes )
+  charset[ bytes[0]&3 ] + bytes[1..21].collect{ charset[ it&63 ] }.join()
+}
 
 
+def strongRandom(){
+  def uuid = new BigInteger(UUID.randomUUID().toString().replace('-',''),16)
+  (21..0).collect{ i -> charset[uuid.shiftRight(i*6) & 63] }.join('')
+}
+
+10.times{ println strongRandom() }
 
