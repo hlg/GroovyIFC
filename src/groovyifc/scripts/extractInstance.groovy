4f311bb64983082fe51f9ec5b1e983f3ad9494a2
hlg <public.void@gmx.de>
2021-11-07 23:12:11 +0100

    remove Linux grep version, disable reindexing

--- src/groovyifc/scripts/extractInstance.groovy
+++ src/groovyifc/scripts/extractInstance.groovy
@@ -7,11 +7,10 @@ if (args.size() < 3) {
   System.exit(1)
 }
 
-boolean reindex = false // There must be a more efficient way using the table to reindex
 def pattern = /#[0-9]+/
 table = [:]
 include = [] as HashSet
-isLinux = false // System.properties['os.name'].toLowerCase().contains('linux')
+reindex = false // There must be a more efficient way using the table to reindex
 
 def schemaPattern = /FILE_SCHEMA\(\((.*)\)\)/
 @Field schema
@@ -20,13 +19,11 @@ originalFile = new File(args[0])
 originalFile.eachLine{ line, i ->
   def schemaMatch = (line.replaceAll("\\s","") =~ schemaPattern).collect{it[1]}
   if (schemaMatch) { schema = schemaMatch[0][1..-2]; return }
-  if(!isLinux){ // TODO fully skip
-    def matches = (line =~ pattern).collect{ it as String } // workaround failing groovy magic
-    if (!matches) return
-    int mi = matches[0][1..-1] as int
-    // println mi
-    table[mi] = [i, matches.size() > 1 ? matches[1..-1].collect{it[1..-1] as int} : []]
-  }
+  def matches = (line =~ pattern).collect{ it as String } // workaround failing groovy magic
+  if (!matches) return
+  int mi = matches[0][1..-1] as int
+  // println mi
+  table[mi] = [i, matches.size() > 1 ? matches[1..-1].collect{it[1..-1] as int} : []]
 }
 
 
@@ -37,45 +34,7 @@ def traverseRefs(root, todo, lookup){
   }
 }
 
-def grepEntity(no, out, err){
-  println "search $no"
-  def proc = ['grep',"$no\\s*=", args[0]].execute()
-  proc.consumeProcessOutput(out, err)
-  proc.waitForOrKill(5000) 
-  proc.exitValue()
-}
-
-data = new StringBuffer()
-entities = args[1].split(',')
-entities.each{ searched ->
-  if (isLinux){
-    println args[0]
-    traverseRefs(searched, {no -> include << no}, {no -> 
-      def out = new StringBuffer()
-      def err = new StringBuffer()
-      if (!grepEntity(no, out, err)){ // exitValue 0: successful grep
-        data << out.toString() // table[no] = [out] // toString?
-        def matches = (out =~ pattern).collect{ it as String } // not necessary here?
-        (matches && matches.size()>1) ? matches[1..-1] - include : []
-      }
-    })
-  } else {
-    def searchedIdx = searched[1..-1] as int
-    if (table[searchedIdx]) {
-      traverseRefs(searchedIdx, {no ->
-        // assert table[no]  
-        include << table[no][0]
-      }, {no -> table[no][1]})
-      println "Finished extracting entity $searched"
-    } else {
-      println "Enitity $searched not found in ${args[0]}"
-    }
-  }
-}
-
-writeExtract( isLinux ? {
-  data.toString().split('\n')
-} : {
+def collectData(){
   includeIt = include.sort().iterator() // should already be sorted
   println "including ${include.size()} elements"
   def current = includeIt.next()
@@ -90,9 +49,9 @@ writeExtract( isLinux ? {
      }
   }
   included
-})
+}
 
-def writeExtract(collectData){
+def writeExtract(){
   def file = new File(args[2])
   def date = new Date().format("yyyy-MM-dd'T'HH:mm:ss", TimeZone.getTimeZone("UTC")) 
   file.withWriter{ writer ->
@@ -110,7 +69,6 @@ DATA;
     if(reindex) include.eachWithIndex{ no, i -> 
        (0..extracted.size()-1).each{ k -> extracted[k] = extracted[k].replaceAll( "$no(\\D)","#${i+1}\$1") } 
     } 
-    */ 
     extracted.each{ writer.writeLine(it) }
     writer << 'ENDSEC;\nEND-ISO-10303-21;'
     println "Finished writing"
@@ -119,4 +77,18 @@ DATA;
 
 // TODO: extract specific instances via some GPath-like syntax, needs proper parser
 
+entities = args[1].split(',')
+entities.each{ searched ->
+  def searchedIdx = searched[1..-1] as int
+  if (table[searchedIdx]) {
+    traverseRefs(searchedIdx, {no ->
+      // assert table[no]  
+      include << table[no][0]
+    }, {no -> table[no][1]})
+    println "Finished extracting entity $searched"
+  } else {
+    println "Enitity $searched not found in ${args[0]}"
+  }
+}
+writeExtract()
 
